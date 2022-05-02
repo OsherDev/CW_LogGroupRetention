4b372745396b2d72e7fa3fd037803eb27aacef4e
Osher Cohen <88276964+OsherDev@users.noreply.github.com>
2022-05-02 16:09:43 +0300

    Update Retention_policy_change.py

--- Retention_policy_change.py
+++ Retention_policy_change.py
@@ -1,5 +1,8 @@
+# Py - 3.9 / timeout in 13 min 
 import json
 import boto3
+import subprocess
+
 
 client = boto3.client('logs')
 newlist=[]
@@ -7,21 +10,23 @@ newlist=[]
 def lambda_handler(event, context):
     response = client.describe_log_groups()
     for logs in response['logGroups']:
-        newlist.append(logs['logGroupName'])
-    print(len(response['logGroups']))
+        newlist.append(logs['logGroupName']) # get the first 50 log groups 
+    while "nextToken" in response:
+        response = client.describe_log_groups(nextToken=response["nextToken"])   # Token for get more than just 50 log groups (limitation of aws) 
+        for logs in response['logGroups']:
+            newlist.append(logs['logGroupName'])
     for i in newlist:
-        if "Int" in i:
-            print(i)
+        if 'Int' in i or 'inegration' in i:
             log=client.put_retention_policy(
                 logGroupName=i,
                 retentionInDays=30
             )
-        elif "stg" in i:
+        elif 'stage' in i or 'Stg' in i or 'Staging' in i:
             log=client.put_retention_policy(
                 logGroupName=i,
                 retentionInDays=90          
             )
-        elif "prod" in i:
+        elif 'Prod' in i or 'Prd' in i or 'production' in i:
             log=client.put_retention_policy(
                 logGroupName=i,
                 retentionInDays=180
